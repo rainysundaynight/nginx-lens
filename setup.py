4c0c8ff5a35350c82d2c8eab6087ca611b17adb2
Даниил Островский <dani.ostrovsky@x5.ru>
2026-01-21 12:04:00 +0300

    Bump up version in setup.py for pypi package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nginx-lens",
-    version="0.3.2",
+    version="0.3.3",
     description="CLI-инструмент для анализа, визуализации и диагностики конфигураций Nginx",
     author="Daniil Astrouski",
     author_email="shelovesuastra@gmail.com",
